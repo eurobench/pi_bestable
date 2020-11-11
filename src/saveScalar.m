6691a639048ee27c190acca050e352a31bf9ffd4
matjazzadravec <matjazzadravec@gmail.com>
2020-11-11 07:59:47 +0100

    Code update and new data output

---
+++ src/saveScalar.m
@@ -0,0 +1,17 @@
+function saveScalar(filename, data)
+################################################################################
+# 'function saveVector(filename, data)'
+#
+# Save vector data into yaml file
+#
+# Copyright BeStable project 2020
+#
+################################################################################
+    
+    fid = fopen(filename, "w");
+    fputs(fid,"type: 'scalar'\n");
+    vector_str = num2str(data, "%1.5f, ");
+    fputs(fid,["value: [" vector_str(1:end-1) "]"]);
+    fclose(fid);
+    
+endfunction
