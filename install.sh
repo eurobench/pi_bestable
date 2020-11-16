18055e5899e754d56e872d787e6f46602edb2838
Anthony Remazeilles <anthony.remazeilles@tecnalia.com>
2020-11-16 22:05:48 +0100

    adjust CI process and testing plan

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ apt-get install -y software-properties-common
 apt-get install -y octave
 apt-get remove -y software-properties-common
 apt-get install -y liboctave-dev
+apt-get install -y octave-statistics
+
 # specific packages required by the code.
 apt-get install -y fig2dev
 
